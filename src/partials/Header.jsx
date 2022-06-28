ca516d595adc0fc538b9d4fc53c0996b43c9d2ce
Rodrigo <dealmeida.qc@gmail.com>
2022-06-28 12:57:14 -0400

    Initial Commit

---
+++ src/partials/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const [top, setTop] = useState(true);
+
+  // detect whether user has scrolled the page down by 10px
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
+
+  return (
+    <header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top && "bg-white backdrop-blur-sm shadow-lg"
+      }`}
+    ></header>
+  );
+}
+
+export default Header;
---
+++ src/partials/Testimonials.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import TestimonialImage from "../images/testimonial.jpg";
+
+function Testimonials() {
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center">
+            <h2 className="h2 mb-4">We want to know what you need</h2>
+            <p className="text-xl text-gray-600" data-aos="zoom-y-out">
+              Find something annoying with grocery shopping? Wish there was an
+              app for it? Send
+            </p>
+          </div>
+          {/* Testimonials */}
+
+          <div
+            className="rounded max-w-2xl mx-auto mt-20 shadow-2 shadow-2xl"
+            data-aos="zoom-y-out"
+          >
+            <div class="contact__form-container">
+              <form
+                action="https://formspree.io/f/moqrzygd"
+                class="contact__form"
+                method="POST"
+              >
+                <div class="contact__form-field">
+                  <label class="contact__form-label" for="name">
+                    Name
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter Your Name"
+                    type="text"
+                    class="contact__form-input"
+                    name="name"
+                    id="name"
+                  />
+                </div>
+                <div class="contact__form-field">
+                  <label class="contact__form-label" for="email">
+                    Email
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter Your Email"
+                    type="text"
+                    class="contact__form-input"
+                    name="email"
+                    id="email"
+                  />
+                </div>
+                <div class="contact__form-field">
+                  <label class="contact__form-label" for="message">
+                    Message
+                  </label>
+                  <textarea
+                    required
+                    cols="30"
+                    rows="4"
+                    class="contact__form-input"
+                    placeholder="Enter Your Message"
+                    name="message"
+                    id="message"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn--theme contact__btn">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
