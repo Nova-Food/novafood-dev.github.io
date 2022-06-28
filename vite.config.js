a2cae5d511d8c7099fdd376b66b6a2485d719e5c
Rodrigo <dealmeida.qc@gmail.com>
2022-06-28 14:24:10 -0400

    update package

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   css: {
     postcss,
   },
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: [
       {
@@ -24,6 +25,6 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  } 
-})
+    },
+  },
+});
